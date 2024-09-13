cf74d63f725a98fe79aef2df6179654cfbb61de9
Sohaila Abo El-Wafa <133037749+Sohaila128@users.noreply.github.com>
2024-09-13 18:43:37 +0300

    Update renderPageLayout.js

--- js/renderPageLayout.js
+++ js/renderPageLayout.js
@@ -172,10 +172,10 @@ function MainFooter() {
       <span class="text-white mx-1 mt-1"> مملكة البحرين</span> 
       </div>
       <div class="">
-        <span class="text-white fw-normal"> Powered By <a target="_blank" href="https://www.linkedin.com/in/sohaila-abo-el-wafa-a2b417248/" class="fw-semibold text-decoration-none fw-normal text-white">  Sohaila Abo El-Wafa</a></span>
+        <span style="white-space:nowrap;" class="text-white fw-normal"> Powered By <a target="_blank" href="https://www.linkedin.com/in/sohaila-abo-el-wafa-a2b417248/" class="fw-semibold text-decoration-none fw-normal text-white">  Sohaila Abo El-Wafa</a></span>
       </div>
       <div class="">
-        <span class="text-white fw-normal"> جميع الحقوق محفوظة لدي   <span class="fw-semibold fw-normal text-white">برك البحرين</span> @2024 </span>
+        <span style="white-space:nowrap;" class="text-white fw-normal"> جميع الحقوق محفوظة لدي   <span class="fw-semibold fw-normal text-white">برك البحرين</span> @2024 </span>
       </div>
     </div>
   </div>
